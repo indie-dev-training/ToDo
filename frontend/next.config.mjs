f64c450273b5f307b40aa3ff46a8a1adb0e24184
m.asano <m.asano@sint.co.jp>
2024-02-04 12:50:54 +0000

    フロントエンド用のディレクトリを作成

---
+++ frontend/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true"
+});
+
+const nextConfig = {
+  reactStrictMode: false,
+  experimental: {
+    webpackBuildWorker: false
+  }
+};
+
+module.exports = withBundleAnalyzer({...nextConfig})
