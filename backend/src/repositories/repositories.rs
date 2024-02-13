b5d5d84a89464317add237492de761630781b50e
potechomp <m.takaku.1223@gmail.com>
2024-02-14 00:29:36 +0900

    repository追加

---
+++ backend/src/repositories/repositories.rs
@@ -0,0 +1,75 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard}
+};
+use serde::{Deserialize, Serialize};
+
+// for create todo
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct NewTodo {
+    text: String
+}
+
+// created todo
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Todo {
+    id: u32,
+    text: String,
+    status: String
+}
+
+impl Todo {
+    fn new(id: u32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            status: String::from("not Ready")
+        }
+    }
+}
+
+pub trait TodoRepository:  Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: NewTodo) -> Todo;
+    fn find(&self, id: u32) -> Option<Todo>;
+    fn find_all(&self) -> Vec<Todo>;
+}
+
+// todo collection
+type Todos = HashMap<u32, Todo>;
+
+// repositories
+#[derive(Debug, Clone)]
+pub struct InMemoryTodoRepository {
+    store: Arc<RwLock<Todos>>
+}
+
+impl InMemoryTodoRepository {
+    fn new() -> Self {
+        Self {
+            store: Arc::default()
+        }
+    }
+
+    // write store(thread safe)
+    fn write_store(&self) -> RwLockWriteGuard<Todos> {
+        self.store.write().unwrap();
+    }
+
+    fn read_store(&self) -> RwLockReadGuard<Todos> {
+        self.store.read().unwrap();
+    }
+}
+
+impl TodoRepository for InMemoryTodoRepository {
+    fn create(&self, payload: NewTodo) -> Todo {
+        todo!();
+    }
+
+    fn find(&self, id: u32) -> Option<Todo> {
+        todo!();
+    }
+
+    fn find_all(&self) -> Vec<Todo> {
+        todo!();
+    }
+}
